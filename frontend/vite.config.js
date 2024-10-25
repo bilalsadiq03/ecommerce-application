12fecfc0e0b7aa16512d093f9cea88425e17ae7a
bilalsadiq03 <bnaq860@gamil.com>
2024-10-25 15:23:58 +0530

    New Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,11 +8,7 @@ export default defineConfig({
     host: true, 
     proxy: {
       '/api': {
-<<<<<<< HEAD
         target: 'http://localhost:8080', 
-=======
-        target: 'https://ecommerce-backend-xsmz.onrender.com', 
->>>>>>> adb9fa60f5ec115ba9c1890b1d0549ff0b61dc75
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },      
