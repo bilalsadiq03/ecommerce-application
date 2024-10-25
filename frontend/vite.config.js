7cffff3c53ec852f48cdf635a6730cbfb6e50d21
Bilal Sadiq <115492050+bilalsadiq03@users.noreply.github.com>
2024-10-25 15:34:00 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: true, 
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', 
+        target: 'https://ecommerce-backend-xsmz.onrender.com', 
 
         changeOrigin: true,
 
