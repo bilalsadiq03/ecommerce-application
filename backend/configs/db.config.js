ee687a1ad0ceda97928dc76b9e9f52f3ec0f6678
bilalsadiq03 <bnaq860@gamil.com>
2024-11-15 20:38:29 +0530

    Updated db configs

--- backend/configs/db.config.js
+++ backend/configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_NAME : "eCommerce_db",
-    DB_URL: "mongodb+srv://bnaq860:JEU7a0QQpe3HyLKt@cluster0.ufprj.mongodb.net/eCommerce_db",
+    DB_URL: "mongodb+srv://bnaq860:JEU7a0QQpe3HyLKt@cluster0.ufprj.mongodb.net/eCommerce_db?ssl=true",
 }
